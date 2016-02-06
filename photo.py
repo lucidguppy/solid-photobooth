0ec9c273cc6f6436befc22f17bc52c73af879f29
Matthew Karas <matthew.james.karas@gmail.com>
2016-02-06 13:54:02 +0000

    Set up gpio to wait for a button press - refactor out button light function

--- photo.py
+++ photo.py
@@ -3,8 +3,22 @@
 import subprocess
 import time
 from Adafruit_I2C import Adafruit_I2C
-trinket_address = 8
+import RPi.GPIO as GPIO
 
+# set up i2c
+trinket_address = 8
 trinket = Adafruit_I2C(trinket_address)
 
-trinket.write8(ord('a'),255)
+# set up gpio
+GPIO.setmode(GPIO.BCM)
+buttonPin = 5
+GPIO.setup(buttonPin, GPIO.IN)
+
+def buttonLight(bright):
+    trinket.write8(ord('a'),bright)
+
+while True:
+    buttonLight(255)
+    GPIO.wait_for_edge(buttonPin, GPIO.FALLING)
+    buttonLight(0)
+    time.sleep(3)
