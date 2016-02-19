522122c6b0719465271f436bdcdcdeec44ef5749
Matthew Karas <matthew.james.karas@gmail.com>
2016-02-19 02:16:32 +0000

    Reduce flash brightness

--- photo.py
+++ photo.py
@@ -79,7 +79,7 @@ def take_picture():
     time.sleep(1)
     bitmap(trinket, flip(onebm), 0, 0, 100)
     time.sleep(1)
-    bitmap(trinket, onbm, 255, 255, 255)
+    bitmap(trinket, onbm, 200, 200, 200)
     filename = "photo" + str(time.time()) + ".jpg"
     subprocess.call(['raspistill', '-t', '1', '-w', '1920', '-h', '1080', '-o', filename])
     time.sleep(0.25)
